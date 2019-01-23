7d2deb4f735c382c6313a5be14283227c7272894
hcollins345 <hcollins345@gmail.com>
2019-01-23 18:53:14 +0100

    part of K-means clustering

--- k-means.py
+++ k-means.py
@@ -23,23 +23,28 @@ def import_vectors():
     except:
         raise ValueError('getDocvectors cannot find or import the presaved models')
  
-qoutesvector = import_vectors()
-#qoutesvector organised as list of ['qoute', array([ docvector]) ]
-vectors = np.array([qoutesvector[x][1] for x in range(len(qoutesvector))])
 
+def plot_cluster_sizes(cluster_num):
+    '''
+    runs and plots K-means from 1 until cluster_num
+    '''
+    qoutesvector = import_vectors()
+    #qoutesvector organised as list of ['qoute', array([ docvector]) ]
+    vectors = np.array([qoutesvector[x][1] for x in range(len(qoutesvector))])
 
-inertias = []
-for i in range(1,101):
-    inertias.append(1000000)
-    print(f'finished cluster = {i}')
-    for j in range(1,5):
-        model = KMeans(n_clusters=i)
-        labels = model.fit_predict(vectors) #shape = n_samples, n_features
-        inertia = model.inertia_
-        if inertia < inertias[-1]:
-            inertias[i-1] = inertia
+    inertias = []
+    for i in range(1,cluster_num+1):
+        inertias.append(1000000)
+        print(f'finished cluster = {i}')
+        for j in range(1,5):
+            model = KMeans(n_clusters=i)
+            labels = model.fit_predict(vectors) #shape = n_samples, n_features
+            inertia = model.inertia_
+            if inertia < inertias[-1]:
+                inertias[i-1] = inertia
 
-x = list(range(1, len(inertias)+1))
-plot = plt.plot(x, inertias)
-plt.savefig('foo.png')
+    x = list(range(1, len(inertias)+1))
+    plot = plt.plot(x, inertias)
+    plt.savefig('foo.png')
+
 
