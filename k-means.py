95ef2766092c022064c0012723f6981c601e990f
hcollins345 <hcollins345@gmail.com>
2019-01-23 20:55:11 +0100

    add return labels to get_model() in k-means

--- k-means.py
+++ k-means.py
@@ -18,12 +18,20 @@ import matplotlib.pyplot as plt
 import os
 import pickle
 
+
+def main():
+    model, labels = get_model(n_clusters = 10, new_model=False, return_labels=True)    
+
+    print(labels)
+
+
 def import_docvectors():
     try:
         docvectors = getDocvectors(None, None)
         return docvectors
     except:
         raise ValueError('getDocvectors cannot find or import the presaved models')
+
 
 def plot_cluster_sizes(cluster_num, name='foo.png'):
     '''
@@ -49,15 +57,25 @@ def plot_cluster_sizes(cluster_num, name='foo.png'):
     plt.savefig(os.getcwd() + '/plots/foo.png')
     print(f'plot saved to {os.getcwd()}' + f"/plots/{name}")
 
-def get_model(name='kmeans', n_clusters=50, new_model=False):
+
+def get_model(name='kmeans', n_clusters=50, new_model=False, return_labels=False):
+    '''
+    Imports trained model along with cluster info.
+    If trained model not available it will create a new model
+    '''
+
     filename = str(n_clusters) + '_cluster_kmeans.p'
     path = os.getcwd() + '/models/' + filename
 
     if os.path.isfile(path) and not new_model:
         'using pretrained model'
         with open(path, "rb") as f:
-            model = pickle.load(f)
-        return model
+            model, labels = pickle.load(f)
+
+        if return_labels:
+            return model, labels
+        else:
+            return model
 
     else:
         doc_vectors = import_docvectors()
@@ -65,27 +83,20 @@ def get_model(name='kmeans', n_clusters=50, new_model=False):
         model = KMeans(n_clusters=n_clusters)
         inertia = 1000000
         for i in range(5):
-            model.fit(raw_vectors)
+            labels = model.fit_predict(raw_vectors)
             if model.inertia_ < inertia:
                 best_model = model
+                best_model_labels = labels
                 inertia = model.inertia_
 
         with open(path, "wb") as f:
-            pickle.dump(best_model, f)
-        return best_model
+            pickle.dump((best_model, best_model_labels), f)
+
+        if return_labels:
+            return best_model, best_model_labels
+        else:
+            return best_model
 
 
-
-
-    # with open(path, "wb") as loc:
-        
-
-model = get_model(n_clusters = 10, new_model=False)    
-
-quotesvectors = import_docvectors()
-raw_vectors = np.array([quotesvectors[x][1] for x in range(len(quotesvectors))])
-
-
-    
-
-
+if __name__ == '__main__':
+    main()
